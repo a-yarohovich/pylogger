e054c7eac77fce478b7fff313067c86210465b3b
Andrew Yarohovich <yarohovich@gmail.com>
2017-12-16 18:57:51 +0300

    add addtional params for stack level

--- logger.py
+++ logger.py
@@ -4,14 +4,13 @@ from logging.handlers import RotatingFileHandler
 from configparser import ConfigParser
 import traceback
 
-# ---------------------------------------------------------------------
 
 class Logger:
     __instance = None
 
     @staticmethod
     def instance():
-        if Logger.__instance == None:
+        if not Logger.__instance:
             Logger.__instance = Logger(os.path.dirname(os.path.realpath(__file__)) + '/logger.cfg')
         return Logger.__instance
 
@@ -40,33 +39,52 @@ class Logger:
             log.setLevel(logging.DEBUG)
         return log, log_file_handler
 
-    def __formatting_msg(self, msg):
-        stack = traceback.extract_stack()
+    @staticmethod
+    def __formatting_msg(msg, filepath_stack_level=-3):
         # Get path and filename without full path
-        filepath = stack[-3][0]
+        stack = traceback.extract_stack()
+        filepath = stack[filepath_stack_level][0]
         filename = filepath.split('/')[-1]
-        line_number = stack[-3][1]
-        funcname = stack[-3][2]
+        line_number = stack[filepath_stack_level][1]
+        funcname = stack[filepath_stack_level][2]
         msg = filename + ":" + str(line_number) + " " + msg + " " + filepath + " in `" + funcname + "`"
         return msg
 
-    def debug(self, msg, *args, **kwargs):
-        self.log.log(logging.DEBUG, self.__formatting_msg(msg), *args, **kwargs)
+    def debug(self, msg, filepath_print_stack_level=-3, *args, **kwargs) -> None:
+        """
+        :param msg: message for print in log
+        :param filepath_print_stack_level: param which define a level of stack for printing filename, line and funkname
+        level must be negative, for example : -2, -5. Default value is -3
+        """
+        self.log.log(logging.DEBUG, self.__formatting_msg(msg, filepath_print_stack_level), *args, **kwargs)
 
-    def info(self, msg, *args, **kwargs):
-        self.log.log(logging.INFO, self.__formatting_msg(msg), *args, **kwargs)
+    def info(self, msg, filepath_print_stack_level=-3, *args, **kwargs) -> None:
+        self.log.log(logging.INFO, self.__formatting_msg(msg, filepath_print_stack_level), *args, **kwargs)
 
-    def warning(self, msg, *args, **kwargs):
-        self.log.log(logging.WARNING, self.__formatting_msg(msg), *args, **kwargs)
+    def warning(self, msg, filepath_print_stack_level=-3, *args, **kwargs) -> None:
+        self.log.log(logging.WARNING, self.__formatting_msg(msg, filepath_print_stack_level), *args, **kwargs)
 
-    def error(self, msg, *args, **kwargs):
-        self.log.log(logging.ERROR, self.__formatting_msg(msg) + "\n" + traceback.format_exc(), *args, **kwargs)
+    def error(self, msg, filepath_print_stack_level=-3, *args, **kwargs) -> None:
+        self.log.log(logging.ERROR, self.__formatting_msg(msg, filepath_print_stack_level) + "\n" + traceback.format_exc(), *args, **kwargs)
 
-    def critical(self, msg, *args, **kwargs):
-        self.log.log(logging.CRITICAL, self.__formatting_msg(msg) + "\n" + traceback.format_exc(), *args, **kwargs)
+    def critical(self, msg, filepath_print_stack_level=-3, *args, **kwargs) -> None:
+        self.log.log(logging.CRITICAL, self.__formatting_msg(msg, filepath_print_stack_level) + "\n" + traceback.format_exc(), *args, **kwargs)
 
 
 # ---------------------------------------------------------------------
 
 # Use this global object for accessing to logger
 LOG = Logger.instance()
+
+
+if __name__ == '__main__':
+    def test0(msg):
+        test1(msg)
+
+    def test1(msg):
+        test2(msg)
+
+    def test2(msg):
+        LOG.debug(msg, -7)
+
+    test0("hello world!")
